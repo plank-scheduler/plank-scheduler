542a052265c5880460a39c90707b6f4688628649
Steve Plank <steve@plankpest.com>
2025-09-24 17:41:20 -0500

    chore: remove backup folder and ignore future backups

--- .cleanup_backup_20250924_121102/debug-env/route.ts
+++
@@ -1,9 +0,0 @@
-// app/api/debug-env/route.ts
-import { NextResponse } from "next/server";
-export function GET() {
-  return NextResponse.json({
-    hasKey: !!process.env.FIELDSTER_API_KEY,
-    baseUrlSet: !!process.env.FIELDSTER_BASE_URL,
-    mock: process.env.USE_FIELDSTER_MOCK,
-  });
-}
