a318a1ed219c680d04b1688609690781380164ac
Steve Plank <steve@plankpest.com>
2026-05-14 15:33:06 -0500

    Use updated scheduler on live booking page

--- pages/booking.tsx
+++ pages/booking.tsx
@@ -1,5 +1,18 @@
-﻿import BookingClient from "../components/BookingClient";
+﻿import dynamic from "next/dynamic";
+import Head from "next/head";
 
-export default function BookPage() {
-  return <BookingClient />;
+const BookingClient = dynamic(() => import("../components/BookingClientTest"), {
+  ssr: false,
+});
+
+export default function BookingPage() {
+  return (
+    <>
+      <Head>
+        <title>Schedule Service | Plank Termite & Pest Control</title>
+      </Head>
+
+      <BookingClient />
+    </>
+  );
 }
